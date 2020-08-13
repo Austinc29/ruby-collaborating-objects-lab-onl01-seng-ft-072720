13a049698aeafa992e7404140f26356ead79cbaa
Austin Cummings <aac729@live.com>
2020-08-13 20:28:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,3 @@
-class Artist
-  attr_accessor :name, :songs
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def self.find_or_create_by_name(name)
-    Artist.new()
-  end
-end
 
 Song.new_by_filename("Action Bronson - Some Song - rap.mp3") 
