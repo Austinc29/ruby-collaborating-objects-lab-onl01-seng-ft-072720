96dfb58a293d862a021ecdfa17ca5f8410184012
Austin Cummings <aac729@live.com>
2020-08-13 20:14:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,11 +16,7 @@ class Artist
   def save
     @@all << self
   end
-  def self.all 
-    self.all.find {|artist| artist.name == name}
-  end 
-
-  def self.find_or_create_by_name(artist_name)
+   def self.find_or_create_by_name(artist_name)
     found_artist = self.all.find {|artist| artist.name == artist_name}
     if found_artist
       found_artist
